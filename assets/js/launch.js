19e916cf08b02e688ee12adb6a998804a85829c1
Pinapple Cash Dev <pineapplecash@protonmail.com>
2021-06-14 03:40:59 -0700

    Ready for launch

--- assets/js/launch.js
+++ assets/js/launch.js
@@ -4,7 +4,7 @@
  */
 window.onload = function() {
   // Month Day, Year Hour:Minute:Second, id-of-element-container
-  countUpFromTime("May 13, 2021 12:00:00", 'countup1'); // ****** Change this line!
+  countUpFromTime("June 14, 2021 02:00:00", 'countup1'); // ****** Change this line!
 };
 function countUpFromTime(countFrom, id) {
   countFrom = new Date(countFrom).getTime();
